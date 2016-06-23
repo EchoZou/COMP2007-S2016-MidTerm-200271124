25b7103303db715f4e004adcaa8e30696474b4ef
Echogogo <zoumo523@gmail.com>
2016-06-23 10:36:01 -0400

    first draft
    almost done ;D

---
+++ COMP2007-S2016-MidTerm-200271124/COMP2007-S2016-MidTerm-200271124/TodoList.aspx.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+// using statements that are required to connect to EF DB
+using COMP2007_S2016_MidTerm_200271124.Models;
+using System.Web.ModelBinding;
+using System.Linq.Dynamic;
+
+namespace COMP2007_S2016_MidTerm_200271124
+{
+    public partial class TodoList : System.Web.UI.Page
+    {
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            // if loading the page for the first time, populate the student grid
+            if (!IsPostBack)
+            {
+                Session["SortColumn"] = "TodoID"; // default sort column
+                Session["SortDirection"] = "ASC";
+                // Get the student data
+                this.GetTodos();
+            }
+        }
+
+        
+
+        protected void GetTodos()
+        {
+            // connect to EF
+            using (TodoConnection db = new TodoConnection())
+            {
+                string SortString = Session["SortColumn"].ToString() + " " + Session["SortDirection"].ToString();
+
+                // query the Students Table using EF and LINQ
+                var Todos = (from allTodos in db.Todos
+                             select allTodos);
+
+                // bind the result to the GridView
+                TodosGridView.DataSource = Todos.AsQueryable().OrderBy(SortString).ToList();
+                TodosGridView.DataBind();
+            }
+        }
+
+       
+        protected void TodosGridView_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            // store which row was clicked
+            int selectedRow = e.RowIndex;
+
+            // get the selected StudentID using the Grid's DataKey collection
+            int TodoID = Convert.ToInt32(TodosGridView.DataKeys[selectedRow].Values["TodoID"]);
+
+            // use EF to find the selected student in the DB and remove it
+            using (TodoConnection db = new TodoConnection())
+            {
+                // create object of the Student class and store the query string inside of it
+                Todo deletedTodo = (from todoRecords in db.Todos
+                                    where todoRecords.TodoID == TodoID
+                                    select todoRecords).FirstOrDefault();
+
+                // remove the selected student from the db
+                db.Todos.Remove(deletedTodo);
+
+                // save my changes back to the database
+                db.SaveChanges();
+
+                // refresh the grid
+                this.GetTodos();
+            }
+        }
+
+        
+        protected void TodosGridView_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            // Set the new page number
+            TodosGridView.PageIndex = e.NewPageIndex;
+
+            // refresh the grid
+            this.GetTodos();
+        }
+
+        protected void PageSizeDropDownList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Set the new Page size
+            TodosGridView.PageSize = Convert.ToInt32(PageSizeDropDownList.SelectedValue);
+
+            // refresh the grid
+            this.GetTodos();
+        }
+
+        protected void TodosGridView_Sorting(object sender, GridViewSortEventArgs e)
+        {
+            // get the column to sorty by
+            Session["SortColumn"] = e.SortExpression;
+
+            // Refresh the Grid
+            this.GetTodos();
+
+            // toggle the direction
+            Session["SortDirection"] = Session["SortDirection"].ToString() == "ASC" ? "DESC" : "ASC";
+        }
+
+        protected void TodosGridView_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            if (IsPostBack)
+            {
+                if (e.Row.RowType == DataControlRowType.Header) // if header row has been clicked
+                {
+                    LinkButton linkbutton = new LinkButton();
+
+                    for (int index = 0; index < TodosGridView.Columns.Count - 1; index++)
+                    {
+                        if (TodosGridView.Columns[index].SortExpression == Session["SortColumn"].ToString())
+                        {
+                            if (Session["SortDirection"].ToString() == "ASC")
+                            {
+                                linkbutton.Text = " <i class='fa fa-caret-up fa-lg'></i>";
+                            }
+                            else
+                            {
+                                linkbutton.Text = " <i class='fa fa-caret-down fa-lg'></i>";
+                            }
+
+                            e.Row.Cells[index].Controls.Add(linkbutton);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
